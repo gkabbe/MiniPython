6f8f8e336144986ff048b487d612b8afd1b358ef
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-11-21 18:03:11 +0100

    Update linalg

--- 22.11.2016/lineare_algebra.py
+++ 22.11.2016/lineare_algebra.py
@@ -5,23 +5,25 @@ import numpy as np
 
 # Vektoren lassen sich folgendermaßen erstellen:
 
-x = np.array([1, 2, 3], dtype=int)
-y = np.array([3, 4, 7], dtype=float)
+x = np.array([1, 2, 3], dtype=int)  # ein Array aus Ganzzahlen (Integern)
+y = np.array([3, 4, 7], dtype=float)  # ein Array aus Gleitkommazahlen (Floats)
 
 print("x =", x)
-
+print()
 print("x + x =", x + x)
-
+print()
 print("x + y =", x + y)
-
+print()
 print("x * y =", x * y)
+print()
 
 print("Skalarprodukt von x und y:", x @ y)  # geht erst ab Python 3.5
 # Alternativ:
 print(np.dot(x, y))
 
 print("Kreuzprodukt:", np.cross(x, y))
-
+print()
+print()
 
 
 # Matrizen funktionieren nach dem selben Prinzip
@@ -53,18 +55,21 @@ print(A * I)
 
 print("A =")
 print(A)
-print("det(A) =")
-print(np.linalg.det(I))
+print("det(A) =", np.linalg.det(I))
 
+print()
+print()
 
 # Auf einzelne Elemente eines Vektors / einer Matrix zugreifen:
 
 x = np.linspace(0, 10, 50)  # dieser Befehl erzeugt einen Array der Länge 50 mit Werten von 0 bis 10
 
-print(x[0])  # gibt das erste Element aus
-print(x[-1]) # gibt das letzte Element aus
+print("x =", x)
+print("x[0] =", x[0])  # gibt das erste Element aus
+print("x[-1] =", x[-1]) # gibt das letzte Element aus
 
-
+print()
+print()
 # Ähnlich bei einer Matrix:
 
 # Folgender Befehl gibt den Eintrag der 0. Zeile und 0. Spalte aus (Nummeriert wird immer von 0 aus!)
@@ -73,6 +78,19 @@ print("A[0, 0] =", A[0, 0])
 print("A[1, -1] =", A[1, -1])
 
 
+# Was, wenn wir auf die gesamte erste Spalte von A zugreifen wollen?
+# -> Slicing
+print("A[:, 0] =", A[:, 0])  #  bedeutet: gib jede Zeile der nullten Spalte aus
+print()
+# Wenn wir nur einen Teil der ersten Spalte wollen, z.B. alles bis auf das letzte Element?
+print("A[:2, 0] =", A[:2, 0])
+print()
+
+# Slicen geht auch in mehreren Dimensionen gleichzeitig:
+# Mache aus der 3x3 Matrix A eine 2x2 Matrix:
+
+print("A[:2, :2] =")
+print(A[:2, :2])
 
 # Aufgabe 1:
 # Schreiben Sie eine eigene Funktion det, die die Determinante einer 2x2 Matrix berechnet
