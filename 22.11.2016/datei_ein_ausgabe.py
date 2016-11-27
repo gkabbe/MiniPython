2a04788b2312e8dca686ab5193ae35587e6c060f
Gabriel Kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-11-27 10:59:54 +0100

    Rename and update dateien_lesen -> datei_ein_ausgabe

---
+++ 22.11.2016/datei_ein_ausgabe.py
@@ -0,0 +1,12 @@
+"""Dateien (die z.B. Messwerte enthalten), lassen sich leicht mit numpy einlesen"""
+
+data = np.loadtxt("messwerte.dat")
+
+
+# Um eine Datei zu speichern, benutzen Sie savetxt
+
+new_data = np.array([[1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9]])
+
+np.savetxt("neue_werte.txt", new_data)
--- 22.11.2016/dateien_lesen.py
+++
@@ -1,3 +0,0 @@
-"""Dateien (die z.B. Messwerte enthalten), lassen sich leicht mit numpy einlesen"""
-
-data = np.loadtxt("messwerte.dat")
